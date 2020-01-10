8a4ae895357f14f4e1e5259fc61b1ed022e67b34
Chris <chrsmith@redventures.com>
2020-01-10 12:38:43 -0500

    Product page style change and details js updated

--- scripts/fetch-product-detail.js
+++ scripts/fetch-product-detail.js
@@ -14,5 +14,13 @@ fetch(`http://localhost:8000/product-detail/${id}`)
     return response.json()
 })
 .then(data => {
-    
+    document.querySelector(".product-name").innerHTML = data.name;
+    document.querySelector(".product-info__img--main").src = data.image_1;
+    document.querySelector(".product-info__p").innerHTML = data.description;
+    document.querySelector(".product-price").innerHTML = data.price;
+    document.querySelector(".product-info__img--small-1").src = data.image_1;
+    document.querySelector(".product-info__img--small-2").src = data.image_1;
+    document.querySelector(".product-info__img--small-3").src = data.image_1;
+    document.querySelector(".product-info__img--small-4").src = data.image_1;
+    document.querySelector(".product-info__img--small-5").src = data.image_1;
 })
