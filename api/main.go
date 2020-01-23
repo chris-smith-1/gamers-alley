a14c33ec6ac7e292d422e39c268a9d44b2bd55ab
Chris <chrsmith@redventures.com>
2020-01-23 14:02:44 -0500

    Products pull from DB and decimals toFixed()

--- api/main.go
+++ api/main.go
@@ -1,62 +1,62 @@
 package main
 
-import(
+import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"encoding/json"
 	// "github.com/davecgh/go-spew/spew"
-	"golang.org/x/crypto/bcrypt"
 	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
 )
 
 //Product EXPORTED
-type Product struct{
-	ProductID int `json:"product_id"`
-	Name string `json:"name"`
-	Description string `json:"description"`
-	Price float64 `json:"price"`
-	Image1 string `json:"image_1"`
-	Image2 string `json:"image_2"`
-	Image3 string `json:"image_3"`
-	Image4 string `json:"image_4"`
-	Image5 string `json:"image_5"`
+type Product struct {
+	ProductID   int     `json:"product_id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Image1      string  `json:"image_1"`
+	Image2      string  `json:"image_2"`
+	Image3      string  `json:"image_3"`
+	Image4      string  `json:"image_4"`
+	Image5      string  `json:"image_5"`
 }
 
 //ProductID EXPORTED
-type ProductID struct{
+type ProductID struct {
 	ProductID int `json:"product_id"`
 }
 
 //User EXPORTED
-type User struct{
-	ID int `json:"id"`
-	Email string `json:"email"`
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 //JWT EXPORTED
-type JWT struct{
+type JWT struct {
 	Token string `json:"token"`
 }
 
 //Error EXPORTED
-type Error struct{
+type Error struct {
 	Message string `json:"message"`
 }
 
 //HELPER FUNCTION
-func respondWithError(w http.ResponseWriter, status int, error Error){
+func respondWithError(w http.ResponseWriter, status int, error Error) {
 	w.WriteHeader(status)
 	fmt.Println(status)
 	json.NewEncoder(w).Encode(error)
 }
 
-func responseJSON(w http.ResponseWriter, data interface{}){
-json.NewEncoder(w).Encode(data)
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
 }
 
 func enableCors(w *http.ResponseWriter) {
@@ -65,32 +65,32 @@ func enableCors(w *http.ResponseWriter) {
 
 //GLOBAL VARIABLES
 var db *sql.DB
- 
-func main() {
-  var err error
-  db, err = sql.Open("mysql", "root:strongpassword!123@tcp(127.0.0.1:3306)/gamers_alley")
-  //os.Getenv in screenshot from Grant. Enter after '"mysql" ,'
- 
-  if err != nil {
-    panic(err.Error())
-  }
- 
-  defer db.Close()
-  err = db.Ping()
- 
-  router := mux.NewRouter()
-  
-  router.HandleFunc("/product-detail/{id}", productDetail).Methods("GET")
-  router.HandleFunc("/products", fetchProducts).Methods("GET")
-  router.HandleFunc("/signup", signup).Methods("POST")
-  router.HandleFunc("/login", login).Methods("POST")
-  router.HandleFunc("/protected", TokenVerifyMiddleware(protectedEndpoint)).Methods("GET")
 
-  log.Println("Listening on port 8000...")
-  log.Fatal(http.ListenAndServe(":8000", router))
+func main() {
+	var err error
+	db, err = sql.Open("mysql", "root:strongpassword!123@tcp(127.0.0.1:3306)/gamers_alley")
+	//os.Getenv in screenshot from Grant. Enter after '"mysql" ,'
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+	err = db.Ping()
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/product-detail/{id}", productDetail).Methods("GET")
+	router.HandleFunc("/products", fetchProducts).Methods("GET")
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleware(protectedEndpoint)).Methods("GET")
+
+	log.Println("Listening on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func productDetail(w http.ResponseWriter, r *http.Request){
+func productDetail(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 
 	var resProd Product
@@ -102,7 +102,7 @@ func productDetail(w http.ResponseWriter, r *http.Request){
 
 	err := row.Scan(&resProd.ProductID, &resProd.Name, &resProd.Description, &resProd.Price, &resProd.Image1, &resProd.Image2, &resProd.Image3, &resProd.Image4, &resProd.Image5)
 
-	if err != nil{
+	if err != nil {
 		respondWithError(w, http.StatusBadRequest, error)
 	}
 
@@ -111,22 +111,37 @@ func productDetail(w http.ResponseWriter, r *http.Request){
 	responseJSON(w, resProd)
 }
 
-func fetchProducts(w http.ResponseWriter, r *http.Request){
+func fetchProducts(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 
-	var products Product
-	
-	stmt := "SELECT product_id, name, image_1 FROM products;"
-	row := db.QueryRow(stmt)
+	var products []Product
 
-	err := row.Scan(&products.ProductID, &products.Name, &products.Image1)
+	stmt := "SELECT product_id, name, price, image_1 FROM products;"
+	rows, err := db.Query(stmt)
 
 	if err != nil {
 		panic(err.Error())
 	}
+
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(&product.ProductID, &product.Name, &product.Price, &product.Image1)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		products = append(products, product)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	responseJSON(w, products)
+	
+	
 }
 
-func signup(w http.ResponseWriter, r *http.Request){
+func signup(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 
 	var user User
@@ -134,26 +149,26 @@ func signup(w http.ResponseWriter, r *http.Request){
 
 	json.NewDecoder(r.Body).Decode(&user)
 
-	if user.Email == ""{
+	if user.Email == "" {
 		error.Message = "Email is missing"
 		respondWithError(w, http.StatusBadRequest, error)
 		return
 	}
-	if user.Password == ""{
+	if user.Password == "" {
 		error.Message = "Password is missing"
 		respondWithError(w, http.StatusBadRequest, error)
 		return
 	}
-	
+
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 
-	if err != nil{
+	if err != nil {
 		panic(err.Error())
 	}
 
 	// fmt.Println("pass text", user.Password)
 	// fmt.Println("hash", hash)
-	
+
 	user.Password = string(hash)
 
 	// fmt.Println(user.Password)
@@ -164,14 +179,14 @@ func signup(w http.ResponseWriter, r *http.Request){
 	db.QueryRow(stmt1, user.Email, user.Password)
 	err = db.QueryRow(stmt2).Scan(&user.ID)
 
-	if err != nil{
+	if err != nil {
 		error.Message = "Server error."
 		respondWithError(w, http.StatusInternalServerError, error)
 		return
 	}
 
 	user.Password = ""
-	
+
 	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(http.StatusCreated)
@@ -180,38 +195,38 @@ func signup(w http.ResponseWriter, r *http.Request){
 }
 
 //GenerateToken EXPORTED
-func GenerateToken(user User)(string, error){
+func GenerateToken(user User) (string, error) {
 	var err error
 	secret := "secret"
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email": user.Email,
-		"iss": "course",
+		"iss":   "course",
 	})
 
 	tokenString, err := token.SignedString([]byte(secret))
 
-	if err != nil{
+	if err != nil {
 		panic(err.Error())
 	}
 
 	return tokenString, nil
 }
 
-func login(w http.ResponseWriter, r *http.Request){
+func login(w http.ResponseWriter, r *http.Request) {
 	var user User
 	var jwt JWT
 	var error Error
 
 	json.NewDecoder(r.Body).Decode(&user)
 
-	if user.Email == ""{
+	if user.Email == "" {
 		error.Message = "Email is missing."
 		respondWithError(w, http.StatusBadRequest, error)
 		return
 	}
 
-	if user.Password == ""{
+	if user.Password == "" {
 		error.Message = "Password is missing."
 		respondWithError(w, http.StatusBadRequest, error)
 		return
@@ -222,12 +237,12 @@ func login(w http.ResponseWriter, r *http.Request){
 	row := db.QueryRow("SELECT * FROM users WHERE email = ?", user.Email)
 	err := row.Scan(&user.ID, &user.Email, &user.Password)
 
-	if err != nil{
-		if err == sql.ErrNoRows{
+	if err != nil {
+		if err == sql.ErrNoRows {
 			error.Message = "The user does not exist"
 			respondWithError(w, http.StatusBadRequest, error)
 			return
-		}else{
+		} else {
 			panic(err.Error())
 		}
 	}
@@ -236,7 +251,7 @@ func login(w http.ResponseWriter, r *http.Request){
 
 	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 
-	if err != nil{
+	if err != nil {
 		error.Message = "Invalid Password"
 		respondWithError(w, http.StatusUnauthorized, error)
 		return
@@ -254,12 +269,12 @@ func login(w http.ResponseWriter, r *http.Request){
 	responseJSON(w, jwt)
 }
 
-func protectedEndpoint(w http.ResponseWriter, r *http.Request){
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("protectedEndpoint invoked")
 }
 
 //TokenVerifyMiddleware EXPORTED
-func TokenVerifyMiddleware(next http.HandlerFunc) http.HandlerFunc{
+func TokenVerifyMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	fmt.Println("TokenVerifyMiddleware invoked")
 	return nil
 }
--- scripts/products.js
+++ scripts/products.js
@@ -1,8 +1,23 @@
+var productContainer = document.querySelector(".product-list");
+
 // Get data from api
-fetch(`http://localhost:8000/products/`)
+fetch(`http://localhost:8000/products`)
 .then(response => {
     return response.json();
 })
 .then(data => {
     console.log(data);
+    for (i=0; i<data.length; i++){
+
+        const newPrice = data[i].price.toFixed(2);
+
+        //Add products to DOM
+        productContainer.innerHTML += `<span class="product-item">
+        <a href="product-detail.html?id=${data[i].product_id}"><img src="${data[i].image_1}" alt="${data[i].name}"></a>
+        <h4><a class="product-item__a-name" href="product-detail.html?id=${data[i].product_id}">${data[i].name}</a></h4>
+        <p>$${newPrice}</p>
+        </span>`
+    }
+
+    console.log(productContainer)
 })
